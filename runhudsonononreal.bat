a023dbf70e310298b96fa9c224368dc39977cc83
mindless <mindless@dev.java.net>
2011-08-13 15:54:53 +0000

    [covcomplplot] Moved to Github

--- runhudsonononreal.bat
+++
@@ -1,6 +0,0 @@
-call mvn clean package
-set MAVEN_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=n
-set HUDSON_HOME=.\test_binary\hudson_home
-rmdir /S /Q test_binary\hudson_home\plugins\covcomplplot
-copy target\covcomplplot.hpi .\test_binary\hudson_home\plugins
-java -jar test_binary\hudson.war
